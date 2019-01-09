c69a2fedc34f4d4b06d6ae10761d84f5188d6e3e
KuotMariak <43444982+KuotMariak@users.noreply.github.com>
2019-01-09 09:54:28 -0500

    Rogue

---
+++ 2D Roguelike(KuotM)/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public BoardManager boardScript;
+
+
+    private int level = 3;
+
+    void Awake()
+    {
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScence(level);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
