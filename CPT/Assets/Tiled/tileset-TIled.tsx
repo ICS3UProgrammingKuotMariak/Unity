8fb78c550ff8226b850d0a2d68691c7f72830693
KuotMariak <43444982+KuotMariak@users.noreply.github.com>
2019-01-18 07:42:26 -0500

    tiled

---
+++ CPT/Assets/Tiled/tileset-TIled.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="tileset-TIled" tilewidth="16" tileheight="16" tilecount="1160" columns="58">
+ <image source="tileset-TIled.png" width="928" height="320"/>
+</tileset>
